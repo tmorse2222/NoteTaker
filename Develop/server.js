e8ecabcd2ee6a7073e9d79b4f61d5338b52f51f9
Tyler M <tmorse2222@gmail.com>
2022-09-26 10:27:08 -0700

    Removed redundant code; Server operational; Landing page with link to notes operational

--- Develop/server.js
+++ Develop/server.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = 3001;
-const api = require('./api');
+const api = require('./routes/index.js');
+const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/api', api);
+app.use(express.static('public'));
+
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'));
@@ -16,15 +17,8 @@ app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
 
-app.get('/api/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '/db/db.json'));
-});
 
-app.post('/api/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '/db/db.json'));
-});
-
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+app.listen(PORT, () => {
+    console.log(`Example app listening at http://localhost:${PORT}`);
     }
 );
