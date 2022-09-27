cb37e1c15309e16fdb5ae52a59bd283437cae5e7
Tyler M <tmorse2222@gmail.com>
2022-09-26 20:58:15 -0700

    Updated comments in server.js

--- Develop/server.js
+++ Develop/server.js
@@ -1,37 +1,38 @@
+// Dependencies
 const express = require('express');
 const path = require('path');
 const app = express();
 const fs = require('fs');
 const PORT = process.env.PORT || 3001;
 const uniqid = require('uniqid');
-
+// express middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
-
-
+// Sets initial page route
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'));
 });
-
+// Sets notes page route
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
-
+// Displays all notes on left side of page
 app.get('/api/notes', (req, res) => {
     const notes = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
     res.json(notes);
 });
-
+// Posts new note to db.json
 app.post('/api/notes', (req, res) => {
     const notes = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
     const newNote = req.body;
+    // Sets id for new note
     newNote.id = uniqid();
     notes.push(newNote);
     fs.writeFileSync('./db/db.json', JSON.stringify(notes));
     res.json(notes);
 });
-
+// Deletes note from db.json
 app.delete('/api/notes/:id', (req, res) => {
     const notes = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
     const noteId = req.params.id;
@@ -39,7 +40,7 @@ app.delete('/api/notes/:id', (req, res) => {
     fs.writeFileSync('./db/db.json', JSON.stringify(newNotes));
     res.json(newNotes);
 });
-
+// initiates server
 app.listen(PORT, () => {
     console.log(`Example app listening at http://localhost:${PORT}`);
     }
